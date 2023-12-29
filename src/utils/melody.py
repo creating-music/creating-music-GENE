83b8e8df1e10aee3b6a743fa5adff57f47a4c753
monognuisy <ysmsmart1@gmail.com>
2023-12-29 23:37:59 +0900

    feat: 코드 기반 멜로디 가중치 설정

--- src/utils/melody.py
+++ src/utils/melody.py
@@ -2,6 +2,7 @@ import pretty_midi
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from pychord import Chord
 
 '''
 Pattern of melody.
@@ -87,6 +88,7 @@ class Melody(MelodyPattern):
             self, 
             scale,
             randomness,
+            start_chord=None,
             bar_length=1,
             division_count=16,
             measure=(4,4),
@@ -98,7 +100,8 @@ class Melody(MelodyPattern):
         self.scale = scale
         self.notes = notes
         self.velocity = velocity
-        self.limit = list(set(Melody.limit).intersection(scale.scale))
+        self.usable_notes = list(set(Melody.limit).intersection(scale.scale))
+        self.start_chord = start_chord
 
         # randomness와 pattern을 동시에 넘겨주면, randomness는 pattern에 영향을 주지 않음.
         # 즉, 주어진 pattern으로 고정.
@@ -120,25 +123,56 @@ class Melody(MelodyPattern):
             if next_note_index in range(max_limit):
                 return next_note_index
 
+    def _choose_from_chord(chord):
+        min_range_num = int(pretty_midi.note_number_to_name(Melody.limit[0])[-1])
+        max_range_num = int(pretty_midi.note_number_to_name(Melody.limit[-1])[-1])
+        chord_octave_range = range(min_range_num, max_range_num + 1)
+
+        while True:
+            note_name = random.choice(chord) + str(random.choice(chord_octave_range))
+            note_number = pretty_midi.note_name_to_number(note_name)
+
+            if (note_number in Melody.limit):
+                return note_number
+
     def build_melody(self):
-        limit_len = len(self.limit)
+        limit_len = len(self.usable_notes)
+        start_chord = self.start_chord
         note_index = 0
+        note_number = 0
         note_len = 1
+
         is_first_note = True
         for i in self.pattern:
             if (i == 0):
                 note_len += 1
                 continue
                
-            # only for the i == 1 case
+            if (is_first_note and start_chord != None):
+                weight = 0.5 if self.scale.has_chord(start_chord) else 1
+
+                # 코드가 스케일에 맞지 않으면 최대한 코드를 중시.
+                # 코드의 음들로 첫 음 결정.
+                if (random.uniform(0, 1) <= weight):
+                    is_first_note = False
+                    note_number = self._choose_from_chord(start_chord)
+                    continue
+                else:
+                    pass    # fall through
+
+            # 첫 음 처리. --> 주어진 코드를 바탕으로 가중치 설정.
+            # 만약 코드가 주어지지 않았다면 무작위로 설정.
             if (is_first_note):
                 is_first_note = False
+
                 note_index = random.choice(range(limit_len))
+                note_number = self.usable_notes[note_index]
                 continue
-           
-            # 전의 note를 append.
+
+            
+            # 이전의 note를 append.
             self.notes.append([
-                self.limit[note_index],
+                note_number,
                 note_len
             ]) 
 
@@ -148,10 +182,11 @@ class Melody(MelodyPattern):
                 self.randomness,
                 limit_len
             )
+            note_number = self.usable_notes[note_index]
         
         # 마지막 note 까지 append
         self.notes.append([
-            self.limit[note_index],
+            note_number,
             note_len
         ]) 
             
@@ -159,16 +194,17 @@ class Melody(MelodyPattern):
         pass
 
     # 주어진 melody, pattern randomness에 따라 음을 약간 변화시킨다.
+    # melody_randomness 의 확률로 원래의 음을 살림.
     def differ_melody(self, melody_randomness, pattern_randomness=0):
-        limit_len = len(self.limit)
+        limit_len = len(self.usable_notes)
         res_melody = []
         for (mel, dur) in self.notes:
             if (random.uniform(0, 1) <= melody_randomness):
                 res_melody.append([mel, dur])
             else:
-                curr_mel_index = self.limit.index(mel)
+                curr_mel_index = self.usable_notes.index(mel)
                 next_mel_index = Melody._calc_next_note(curr_mel_index, self.randomness, limit_len)
-                next_mel = self.limit[next_mel_index]
+                next_mel = self.usable_notes[next_mel_index]
                 res_melody.append([next_mel, dur])
 
         self.notes = res_melody
@@ -180,6 +216,7 @@ class Scale:
             scale
         ):
         self.root = root
+        self.default_scale = scale
         self.build_scale(scale)
 
     def build_scale(self, scale):
@@ -192,6 +229,43 @@ class Scale:
         for w in weight:
             result_scale.extend(main_scale + root_as_number - w)
         self.scale = set(result_scale)
+
+    def has_note(self, note_name):
+        '''
+        스케일에 해당 음이 있는지 확인.
+        '''
+
+        OCTAVE = 12
+        notes_with_sharp = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+        notes_with_flat  = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab']
+
+        if (note_name in notes_with_sharp):
+            idx = notes_with_sharp.index(note_name)
+        else:
+            idx = notes_with_flat.index(note_name)
+    
+        if (self.root in notes_with_sharp):
+            root_idx = notes_with_sharp.index(self.root)
+        else:
+            root_idx = notes_with_flat.index(self.root)
+
+        diff = idx - root_idx
+        if (diff < 0):
+            diff + OCTAVE
+
+        return diff in self.default_scale
+        
+    def has_chord(self, chord: Chord):
+        '''
+        주어진 코드가 스케일 내에 있는지 확인.
+        '''
+        
+        res = True
+        for note in chord.components():
+            res = res and self.has_note(note)
+            
+        return res
+            
 
 class MajorScale(Scale):
     def __init__(
@@ -227,5 +301,5 @@ if __name__ == '__main__':
         start_base += d/4
 
     print(np.vectorize(pretty_midi.note_number_to_name)(np.array(melody_main)), melody_dur)
-    output_midi.instruments.append(main_piano)
-    output_midi.write('src/test/output.mid')
+    # output_midi.instruments.append(main_piano)
+    # output_midi.write('src/test/output.mid')
