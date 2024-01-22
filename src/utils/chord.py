ed213b1b939216c4867eed96f7d2d62bbe429c88
monognuisy <ysmsmart1@gmail.com>
2024-01-22 17:22:00 +0900

    feat: initialize chord progressions

---
+++ src/utils/chord.py
@@ -0,0 +1,21 @@
+from pychord import ChordProgression
+
+chord_progressions = [
+    ChordProgression('Dm7', 'G7', 'CM7', 'CM7'),
+    ChordProgression('Dm7', 'G7', 'CM7', 'Am7'),
+    ChordProgression('Dm7', 'G7', 'CM7', 'A7'),
+    ChordProgression('FM7', 'G7', 'Em7', 'Am7'),
+    ChordProgression('FM7', 'Em7', 'Dm7', 'Em7'),
+    ChordProgression('CM7', 'G7', 'Am7', 'FM7'),
+    ChordProgression('CM7', 'Am7', 'FM7', 'G7'),
+    ChordProgression('CM7', 'FM7', 'G7', 'Am7'),
+    ChordProgression('C7', 'C7', 'F7', 'F7'),
+    ChordProgression('CM7', 'G7', 'Am7', 'Em7',
+                     'FM7', 'CM7', 'FM7', 'G7'),
+    ChordProgression('Am7', 'FM7', 'CM7', 'G7'),
+    ChordProgression('Am7', 'FM7', 'G7', 'CM7'),
+    ChordProgression('Am7', 'Dm7', 'Em7', 'Am7'),
+    ChordProgression('Am7', 'G7', 'FM7', 'G7'),
+    ChordProgression('CM7', 'E7', 'FM7', 'Fm7'),
+    ChordProgression('CM7', 'BbM7', 'FM7', 'Fm7'),
+]
