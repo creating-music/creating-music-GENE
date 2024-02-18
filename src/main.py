879863804ae14c764284807b68bd59810b926e8a
monognuisy <ysmsmart1@gmail.com>
2024-02-18 17:16:13 +0900

    feat: make song with one function call

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import numpy as np
 from utils.chord import *
 from utils.melody import *
 
-bpm = 80
+# bpm = 80
 
 class NoteWrapper:
     def __init__(self, notes, division):
@@ -87,6 +87,7 @@ def create_part(
 def merge_part(
     part_list: list[list[NoteWrapper]],
     instrument_list: list[pretty_midi.Instrument],
+    bpm: int,
 ):
     start_base = 0
 
@@ -102,13 +103,74 @@ def merge_part(
 
         start_base = start_base_each
 
-if __name__ == '__main__':
-    output_midi = pretty_midi.PrettyMIDI()
-    main_piano = pretty_midi.Instrument(program=0)
-    sub_piano = pretty_midi.Instrument(program=0)
+def make_song(
+    genre: str,
+    mood: str,
+    bpm: int | None=None,
+    max_randomness: int=0.7,
+):
+    if genre not in ['newage', 'retro']:
+        raise Exception('Unsupported genre.')
 
+    if mood not in ['happy', 'sad', 'grand']:
+        raise Exception('Unsupported mood.')
+
+    quant_size = 0.1
+    limitations = {
+        'newage': {
+            'bpm': set(range(60, 120 + 1)),
+            'randomness': set(np.arange(0.2, 0.6, quant_size)),
+        },
+        'retro': {
+            'bpm': set(range(80, 160 + 1)),
+            'randomness': set(np.arange(0.2, 0.9, quant_size)),
+        },
+        'happy': {
+            'bpm': set(range(80, 160 + 1)),
+            'randomness': set(np.arange(0.1, 0.9, quant_size)),
+        },
+        'sad': {
+            'bpm': set(range(60, 100 + 1)),
+            'randomness': set(np.arange(0.1, 0.8, quant_size)),
+        },
+        'grand': {
+            'bpm': set(range(60, 90 + 1)),
+            'randomness': set(np.arange(0.1, 0.8, quant_size)),
+        },
+    }
+
+    if bpm is None:
+        # 장르와 무드에 따라 bpm 설정
+        bpm_list = list(limitations[genre]['bpm'].intersection(limitations[mood]['bpm']))
+        bpm = random.choice(bpm_list)
+
+    randomness_list = list(limitations[genre]['randomness'].intersection(limitations[mood]['randomness']))
+
+    rand_limit_high = min(max_randomness, randomness_list[-1])
+    randomness_list = np.linspace(randomness_list[0], rand_limit_high, 50)
+
+    randomness_selection = sorted([
+        random.choice(randomness_list),
+        random.choice(randomness_list),
+        random.choice(randomness_list),
+        random.choice(randomness_list),
+    ])
+
+    print(randomness_selection, bpm)
+
+    instruments = {
+        'newage': [pretty_midi.Instrument(program=0), pretty_midi.Instrument(program=0)],
+        'retro': [pretty_midi.Instrument(program=80), pretty_midi.Instrument(program=81)],
+    }
+
+    # song making start
+    output_midi = pretty_midi.PrettyMIDI()
+    [main_instrument, sub_instrumnet] = instruments[genre]
+
+    # TODO: randomize scale
     default_scale = MajorScale('C')
 
+    # TODO: transpose chord w.r.t. root
     inoutro = create_part(
         scale=default_scale,
         chord_pattern=ChordWithPattern(
@@ -116,7 +178,7 @@ if __name__ == '__main__':
             pattern=ArpeggioPattern(pat_method='one-five', dur_method='stacato'),
             division=8,
         ),
-        randomness=0.2,
+        randomness=randomness_selection[0],
         bar_part=4,
         measure=(4,4),
     )
@@ -127,7 +189,7 @@ if __name__ == '__main__':
             pattern=ArpeggioPattern(pat_method='one-five', dur_method='stacato'),
             division=8,
         ),
-        randomness=0.4,
+        randomness=randomness_selection[1],
         bar_part=8,
         measure=(4,4),
     )
@@ -138,7 +200,7 @@ if __name__ == '__main__':
             pattern=ArpeggioPattern(pat_method='one-five', dur_method='stacato'),
             division=8,
         ),
-        randomness=0.5,
+        randomness=randomness_selection[2],
         bar_part=8,
         measure=(4,4),
     )
@@ -149,16 +211,23 @@ if __name__ == '__main__':
             pattern=ArpeggioPattern(pat_method='one-five', dur_method='stacato'),
             division=8,
         ),
-        randomness=0.7,
+        randomness=randomness_selection[3],
         bar_part=8,
         measure=(4,4),
     )
 
     merge_part(
         part_list=[inoutro, verse, chorus, verse, chorus, bridge, chorus, inoutro],
-        instrument_list=[main_piano, sub_piano],
+        instrument_list=[main_instrument, sub_instrumnet],
+        bpm=bpm,
     )
 
-    output_midi.instruments.append(main_piano)
-    output_midi.instruments.append(sub_piano)
+    output_midi.instruments.append(main_instrument)
+    output_midi.instruments.append(sub_instrumnet)
     output_midi.write('src/test/output.mid')
+
+if __name__ == '__main__':
+    make_song(
+        genre='newage',
+        mood='happy',
+    )
